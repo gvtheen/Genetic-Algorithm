c6528c7f8e3b118e422007ec77d118c6507dbc03
gvtheen <glzhuang@zjut.edu.cn>
2019-08-13 23:18:42 +0800

    update

--- main.cpp
+++ main.cpp
@@ -6,14 +6,11 @@ using namespace GAZJUT;
 int main()
 {
     CGaparameter* _ga_para = new CGaparameter();
-    //cout << "Hello world-1!" << endl;
-    CGAEngine* _ga_Engine= new CGAEngine(_ga_para);
+       CGAEngine* _ga_Engine= new CGAEngine(_ga_para);
 
-    //cout << "Hello world-2!" << endl;
     _ga_Engine->init();
     _ga_Engine->evolve();
     delete _ga_Engine;
     delete _ga_para;
-    cout << "Hello world!" << endl;
     return 0;
 }
